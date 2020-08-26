90019de21725b77788572a85c979e7bf07d1ed11
Dan Burton <danburton720@hotmail.com>
2020-08-26 13:48:34 +0100

    Added api/student/{studentId} endpoint, and added custom and generic exception handling

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,18 +16,39 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// define endpoint for "/students"
-	@GetMapping("/students")
-	public List<Student> getStudents() {
+	private List<Student> theStudents;
+	
+	// define @PostConstruct to load the student data ...only once when Bean is constructed
+	@PostConstruct
+	public void loadData() {
 		
-		List<Student> theStudents = new ArrayList<>();
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Poornima", "Patel"));
 		theStudents.add(new Student("Mario", "Rossi"));
 		theStudents.add(new Student("Mary", "Smith"));
 		
+	}
+	
+	// define endpoint for "/students"
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		
 		return theStudents;
 		
+	}
+	
+	// define endpoint for "/students/{studentId} - return student at index
+	@GetMapping("/students/{studentId}")
+	public Student getStudentById(@PathVariable int studentId) {
+		
+		// check the studentId against list size
+		if (studentId >= theStudents.size() || studentId < 0) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		
+		// just index into the list	
+		return theStudents.get(studentId);
 	}
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,5 +12,8 @@
 	<hr>
 	
 	<a href="${pageContext.request.contextPath}/test/hello">Hello</a> 
+	<br><br>
+	<a href="${pageContext.request.contextPath}/api/students">Get all students</a> 
+	
 </body>
 </html>
